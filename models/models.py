04fe977929f3e3e5469d07a274c4bf8cf6524861
Edgard Pimentel <pimentelrojas@gmail.com>
2016-06-28 23:13:56 -0500

    adaptation to v8

--- models/models.py
+++ models/models.py
@@ -6,33 +6,30 @@ from openerp.tools import float_is_zero, float_compare
 
 
 class sale_order_discount(models.Model):
-    _description = 'Sale order global discount'
     _inherit = 'sale.order'
-     
+
     discount_type = fields.Selection(
                                      [('percent','Percentage'),
                                       ('amount','Amount')],
                                       string = 'Discount Type',
                                       help = 'Select discount type',
                                       default = 'percent')
-    discount_rate = fields.Float( string = 'Discount Rate', default = '0.0', store = True)
-    
-    amount_discount = fields.Float( string = 'Total Global Discount', compute='_compute_discount', store = True)    
-    
+    discount_rate = fields.Float('Discount Rate', default = '0.0')
+
+    amount_discount = fields.Float('Total Global Discount', compute='_compute_discount')
+
 
     @api.one
     @api.depends('discount_type','discount_rate','amount_total')
     def _compute_discount(self):
-
         mod_obj = self.env['ir.model.data']
         amount_discount = 0.0
         if self.discount_type == 'percent':
             amount_discount = self.amount_untaxed * self.discount_rate / 100
         else:
             amount_discount = self.discount_rate
-    
+
         self.amount_discount = amount_discount
-    
 
 
     @api.depends('order_line.price_total')
@@ -41,7 +38,7 @@ class sale_order_discount(models.Model):
         """
         Compute the total amounts of the SO.
         """
-       
+
         for order in self:
             amount_untaxed = amount_tax = amount_discount = 0.0
             for line in order.order_line:
@@ -51,44 +48,43 @@ class sale_order_discount(models.Model):
                 amount_discount = amount_untaxed * self.discount_rate / 100
             else:
                 amount_discount = self.discount_rate
-        
+
             order.update({
                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
                 'amount_discount': order.pricelist_id.currency_id.round(amount_discount),
                 'amount_total': amount_untaxed - amount_discount + amount_tax,
             })
-            
 
-            
+
+
     @api.model
     def _prepare_invoice(self):
         """
-        This method send the discount_type, discount_rate and amount_discount to the 
+        This method send the discount_type, discount_rate and amount_discount to the
         account.invoice model
         """
         res = super(sale_order_discount, self)._prepare_invoice()
         res['discount_type'] = self.discount_type
         res['discount_rate'] = self.discount_rate
-            
+
         return res
-    
-            
+
+
 class invoice_global_discount(models.Model):
-    _description = 'Invoice global discount'
     _inherit = 'account.invoice'
-     
+
     discount_type = fields.Selection(
                                      [('percent','Percentage'),
                                       ('amount','Amount')],
                                       string = 'Discount Type',
                                       help = 'Select discount type',
                                       default = 'percent')
-    discount_rate = fields.Float( string = 'Discount Rate', default = '0.0', store = True)
-    
-    amount_discount = fields.Monetary( string = 'Total Global Discount', compute='_compute_discount', store = True) 
-    
-    
+    discount_rate = fields.Float('Discount Rate', default = '0.0')
+
+    amount_discount = fields.Float('Total Global Discount', compute='_compute_discount', store = True)
+
+
     @api.one
     @api.depends('discount_type','discount_rate','amount_total')
     def _compute_discount(self):
@@ -99,25 +95,25 @@ class invoice_global_discount(models.Model):
             amount_discount = self.amount_untaxed * self.discount_rate / 100
         else:
             amount_discount = self.discount_rate
-    
-        self.amount_discount = amount_discount    
-    
+
+        self.amount_discount = amount_discount
+
 
     @api.one
-    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id')
+    @api.depends('invoice_line.price_subtotal', 'tax_line.amount', 'currency_id', 'company_id')
     def _compute_amount(self):
 
-        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
-        self.amount_tax = sum(line.amount for line in self.tax_line_ids)
+        self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line)
+        self.amount_tax = sum(line.amount for line in self.tax_line)
         amount_discount = amount_total = 0.0
         if self.discount_type == 'percent':
             amount_discount = self.amount_untaxed * self.discount_rate / 100
         else:
             amount_discount = self.discount_rate
-                
+
         amount_total = self.amount_untaxed - amount_discount + self.amount_tax
         self.amount_total = amount_total
-        
+
         amount_total_company_signed = amount_total
         amount_untaxed_signed = self.amount_untaxed
         if self.currency_id and self.currency_id != self.company_id.currency_id:
@@ -130,15 +126,15 @@ class invoice_global_discount(models.Model):
 
     @api.one
     @api.depends(
-        'state', 'currency_id', 'invoice_line_ids.price_subtotal',
-        'move_id.line_ids.amount_residual',
-        'move_id.line_ids.currency_id')
+        'state', 'currency_id', 'invoice_line.price_subtotal',
+        'move_id.line_id.amount_residual',
+        'move_id.line_id.currency_id')
     def _compute_residual(self):
         residual = 0.0
         residual_company_signed = 0.0
         sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
-        for line in self.sudo().move_id.line_ids:
-            if line.account_id.internal_type in ('receivable', 'payable'):
+        for line in self.sudo().move_id.line_id:
+            if line.account_id.type in ('receivable', 'payable'):
                 residual_company_signed += line.amount_residual
                 if line.currency_id == self.currency_id:
                     residual += line.amount_residual_currency if line.currency_id else line.amount_residual
