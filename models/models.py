507ed0b373f844b4ca71d4901be1d27f0a724bc7
Edgard Pimentel <pimentelrojas@gmail.com>
2016-07-12 21:01:14 -0500

    update models.py

--- models/models.py
+++ models/models.py
@@ -32,30 +32,29 @@ class sale_order_discount(models.Model):
         self.amount_discount = amount_discount
 
 
-    @api.depends('order_line.price_total')
-    @api.multi
-    def _amount_all(self):
+    #@api.depends('order_line.price_subtotal')
+    #@api.multi
+    #def _amount_all(self):
         """
         Compute the total amounts of the SO.
         """
+    #    res = {}
+    #    for order in self:
+    #        amount_untaxed = amount_tax = amount_discount = 0.0
+    #        for line in order.order_line:
+    #            amount_untaxed += line.price_subtotal
+    #            amount_tax += line.price_tax
+    #        if self.discount_type == 'percent':
+    #            amount_discount = amount_untaxed * self.discount_rate / 100
+    #        else:
+    #            amount_discount = self.discount_rate
 
-        for order in self:
-            amount_untaxed = amount_tax = amount_discount = 0.0
-            for line in order.order_line:
-                amount_untaxed += line.price_subtotal
-                amount_tax += line.price_tax
-            if self.discount_type == 'percent':
-                amount_discount = amount_untaxed * self.discount_rate / 100
-            else:
-                amount_discount = self.discount_rate
-
-            order.update({
-                'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
-                'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
-                'amount_discount': order.pricelist_id.currency_id.round(amount_discount),
-                'amount_total': amount_untaxed - amount_discount + amount_tax,
-            })
-
+            #order.update({
+    #        self.amount_untaxed = order.pricelist_id.currency_id.round(amount_untaxed)
+    #        self.amount_tax = order.pricelist_id.currency_id.round(amount_tax)
+    #        self.amount_discount = order.pricelist_id.currency_id.round(amount_discount)
+    #        self.amount_total = amount_untaxed - amount_discount + amount_tax
+            #})
 
 
     @api.model
@@ -82,13 +81,12 @@ class invoice_global_discount(models.Model):
                                       default = 'percent')
     discount_rate = fields.Float('Discount Rate', default = '0.0')
 
-    amount_discount = fields.Float('Total Global Discount', compute='_compute_discount', store = True)
+    amount_discount = fields.Float('Total Global Discount', compute='_compute_discount', store=True)
 
 
     @api.one
     @api.depends('discount_type','discount_rate','amount_total')
     def _compute_discount(self):
-
         mod_obj = self.env['ir.model.data']
         amount_discount = 0.0
         if self.discount_type == 'percent':
